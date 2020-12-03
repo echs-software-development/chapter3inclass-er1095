cee98004b40c643129fd50551704379206276b5e
EMILY RAMIREZ <er1095@students.necsd.net>
2020-12-03 15:18:34 +0000

    all problems are done

--- Prob3.java
+++ Prob3.java
@@ -2,9 +2,32 @@ import java.util.Scanner;
 
 public class Prob3  {
   public static void main(String[] args){
-    
+  Scanner scan = new Scanner(System.in);
+  System.out.println("Enter a number between 0-4");
+  // gets number from user
+  int num = scan.nextInt(); 
+  // translates number to a letter 
+  if (num == 0) {
+  System.out.println("F");
+  }
 
+  else if (num == 1){
+  System.out.println("D");
+  }
 
+  else if (num == 2) {
+  System.out.println("C");
+  }
+
+  else if (num == 3) {
+  System.out.println("B");
+  }
+
+  else {
+  System.out.println("A");
+  }
+
+  
     
   }
 }
--- Prob4.java
+++ Prob4.java
@@ -2,6 +2,22 @@ import java.util.Scanner;
 
 public class Prob4  {
   public static void main(String[] args){
+  Scanner scan = new Scanner(System.in);
+  System.out.println("What is the temperature?");
+
+  double temp = scan.nextDouble();
+  //  If the temperature falls below 99 or above 102 your code should print “WARNING”
+  if (temp <= 99 || temp >= 102)
+  System.out.println("WARNING");
+
+  else{
+    System.out.println("Temperature is ok");
+  }
+
+  
+
+
+
     
 
 
